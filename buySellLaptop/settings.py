20502a879ad44526486480d63d54756e1212dcad
Nahom d <nahom.d54@gmail.com>
2025-01-05 22:41:04 +0300

    feat: add environment variables for Telegram API integration

--- buySellLaptop/settings.py
+++ buySellLaptop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'laptops',
 ]
+
+TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
+TELEGRAM_SESSIONS = os.getenv("TELEGRAM_SESSIONS")
+TELEGRAM_CHANNELS = os.getenv("TELEGRAM_CHANNELS", "").split(",")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
