28e768f980f7fd59052fa8a27eca605c0a1c463b
Nahom d <nahom.d54@gmail.com>
2025-01-06 17:06:45 +0300

    feat: implement asynchronous laptop scraping with error handling and logging, and add task scheduler for periodic execution

--- laptops/tasks.py
+++ laptops/tasks.py
@@ -1,9 +1,22 @@
+import asyncio
 from pyrogram import Client
 from django.conf import settings
+from django.utils import timezone
 from .models import LaptopPost
+from pyrogram.errors import FloodWait
 import re
 import json
 import google.generativeai as genai
+
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from asgiref.sync import sync_to_async
+
+import logging
+
+# Get the logger for your app
+logger = logging.getLogger("laptops")
 
 
 ai_system_prompt = """
@@ -38,7 +51,6 @@ If any details are missing in the input, use null or an empty string. Ensure the
 """
 
 
-
 genai.configure(api_key=settings.GEMENI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=ai_system_prompt)
 
@@ -62,31 +74,32 @@ def verify_laptop_dict(data):
         "price": (str, type(None)),  # Price can be a string or None
     }
 
-
     cleaned_data = {}
-    
+
     # Validate and clean the data
     for key, expected_type in expected_structure.items():
         if key in data and isinstance(data[key], expected_type):
             cleaned_data[key] = data[key]
         elif key in data:
-            return False, f"Key '{key}' has invalid type. Expected {expected_type}, got {type(data[key])}"
+            return (
+                False,
+                f"Key '{key}' has invalid type. Expected {expected_type}, got {type(data[key])}",
+            )
         else:
             return False, f"Missing required key: {key}"
 
-    return True, cleaned_data 
-
-
+    return True, cleaned_data
 
 
 def get_json_response(data):
-    pattern = r'\{[^{}]*\}'
+    pattern = r"\{[^{}]*\}"
     match = re.findall(pattern, data)
     return match[0] if match else {}
 
 
 def process_product(product):
-    
+    if not product:
+        return False, {}
     response = model.generate_content(product)
     json_response = get_json_response(response.text)
 
@@ -96,21 +109,94 @@ def process_product(product):
 
     return status, verified_product
 
-    
+
+async def scrape_laptops_async():
+    app = Client(
+        "LaptopScraper",
+        api_id=settings.TELEGRAM_API_ID,
+        api_hash=settings.TELEGRAM_API_HASH,
+        session_string=settings.TELEGRAM_SESSIONS,
+    )
+    channels = settings.TELEGRAM_CHANNELS
+
+    max_messages = 20
+    messages_with_captions = 0
+    last_message_id = 0
+
+    try:
+        async with app:
+            for channel in channels:
+                while messages_with_captions < max_messages:
+                    try:
+                        async for message in app.get_chat_history(
+                            channel, limit=50, offset_id=last_message_id
+                        ):
+                            if message.caption:
+                                logger.info(f"Message: {message.caption}")
+                                messages_with_captions += 1
+
+                            caption = message.caption
+                            status, processed_product = process_product(caption)
+                            logger.info(
+                                f"Processed product: {processed_product}, {caption}"
+                            )
+                            if status:
+                                try:
+                                    await sync_to_async(
+                                        LaptopPost.objects.update_or_create
+                                    )(
+                                        channel_name=message.sender_chat.title
+                                        if message.sender_chat
+                                        else "Unknown",
+                                        posted_at=timezone.make_aware(message.date),
+                                        post_id=message.id,
+                                        defaults=processed_product,
+                                        channel_id=message.sender_chat.id,
+                                    )
+                                except Exception as e:
+                                    logger.error(
+                                        f"Error saving product to database: {str(e)}"
+                                    )
+                                    raise e
+                            else:
+                                logger.error(
+                                    f"Error processing product: {processed_product}"
+                                )
+
+                            last_message_id = message.id
+                            if len(messages_with_captions) >= max_messages:
+                                break
+
+                    except FloodWait as e:
+                        print(f"Rate limit exceeded. Waiting for {e.value} seconds...")
+                        await asyncio.sleep(
+                            e.value
+                        )  # Wait for the required time before retrying
+                        continue
+
+                    if last_message_id is None:
+                        break
+
+                # customize based on the channel so i might need to make it dynamic
+
+    except ValueError as e:
+        logger.error(f"Error processing channel {channel}: {str(e)}")
+    except KeyError as e:
+        logger.error(f"KeyError processing channel {channel}: {str(e)}")
+    except Exception as e:
+        logger.error(f"Unexpected error processing channel {channel}: {str(e)}")
+
 
 def scrape_laptops():
-    app = Client(settings.TELEGRAM_SESSIONS, api_id=settings.TELEGRAM_API_ID, api_hash=settings.TELEGRAM_API_HASH)
-    channels = settings.TELEGRAM_CHANNELS
-    with app:
-        for channel in channels:
-            messages = app.get_chat_history(channel, limit=50)
-            for message in messages:
-                # customize based on the channel so i might need to make it dynamic
-                status, processed_product = process_product(message)
-                if status:
-                    LaptopPost.objects.update_or_create(
-                        channel_name=message.sender_chat,
-                        posted_at=message.date,
-                        post_id=message.id,
-                        defaults=processed_product
-                    )
+    asyncio.run(scrape_laptops_async())
+
+
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        scrape_laptops,
+        trigger=IntervalTrigger(minutes=30),
+        id="Scrapelaptops",
+        replace_existing=True,
+    )
+    scheduler.start()
