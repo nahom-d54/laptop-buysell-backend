37779b415c6796c37033713afe56a20e16fb7c94
Nahom d <nahom.d54@gmail.com>
2025-01-06 17:07:01 +0300

    feat: refactor LaptopPostListView to LaptopResourceView using ReadOnlyModelViewSet and add pagination support

--- laptops/views.py
+++ laptops/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import LaptopPost
 from .serializers import LaptopPostSerializer
+from rest_framework.pagination import PageNumberPagination
 
-class LaptopPostListView(generics.ListAPIView):
+
+class LaptopResourceView(ReadOnlyModelViewSet):
     queryset = LaptopPost.objects.all()
     serializer_class = LaptopPostSerializer
+    permission_classes = []
+    authentication_classes = []
+    pagination_class = PageNumberPagination
