68a9b872ab1138f778132635afd32c3010c8bfc2
Nahom d <nahom.d54@gmail.com>
2025-01-06 17:06:09 +0300

    feat: initialize task scheduler in Laptops app configuration

--- laptops/apps.py
+++ laptops/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class LaptopsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'laptops'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "laptops"
+
+    def ready(self):
+        from laptops import tasks
+
+        tasks.start_scheduler()
