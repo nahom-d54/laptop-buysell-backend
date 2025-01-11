ba256389616c00cb7b1cf2037e163b93808588e3
Nahom d <nahom.d54@gmail.com>
2025-01-11 19:30:55 +0300

    feat: initialize scheduler on app ready

--- laptops/apps.py
+++ laptops/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class LaptopsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "laptops"
+
+    def ready(self):
+        from laptops.tasks import start_scheduler
+
+        start_scheduler()
