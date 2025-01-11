63601a87d105f58fc07b3520b9e7396e18b0f6f5
Nahom d <nahom.d54@gmail.com>
2025-01-11 19:31:05 +0300

    fix: replace scheduler initialization with laptop scraping functionality

--- laptops/management/commands/scraper_command.py
+++ laptops/management/commands/scraper_command.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
-from laptops.tasks import start_scheduler
+from laptops.tasks import scrape_laptops
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         # Put here some script to get the data from api service and store it into your models.
-        start_scheduler()
+        scrape_laptops()
