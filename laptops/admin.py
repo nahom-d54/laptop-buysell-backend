9a8f062f901433ce829d0e6e578939cc02cc8f54
Nahom d <nahom.d54@gmail.com>
2025-01-15 21:31:53 +0300

    feat: register SimilarityScore model in admin panel

--- laptops/admin.py
+++ laptops/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from laptops.models import TelegramChat, LaptopPost
+from laptops.models import TelegramChat, LaptopPost, SimilarityScore
 
 # Register your models here.
-admin.site.register([TelegramChat, LaptopPost])
+admin.site.register([TelegramChat, LaptopPost, SimilarityScore])
